2c965374b20cf9c6de91fdb027d3f275ee495688
shepard <koshimyaka@gmail.com>
2026-01-21 14:47:50 +0300

    Удалил все файлы в ветке homework-2.8

--- BaseTest.java
+++
@@ -1,9 +0,0 @@
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
-
-public class BaseTest {
-    @BeforeAll
-    static void getBaseUri(){
-        RestAssured.baseURI = "https://postman-echo.com";
-    }
-}
